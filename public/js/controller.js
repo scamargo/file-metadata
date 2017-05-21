3b93d223506e64769b805976eec9f2d2260a8401
Sergio Camargo <sergio.camargo@utexas.edu>
2017-05-21 00:24:00 +0000

    Submit file through ajax

---
+++ public/js/controller.js
@@ -0,0 +1,30 @@
+$(document).ready(function(event) {
+    // process the form
+    $("form#data").submit(function() {
+
+        var formData = new FormData(this);
+        
+        // process the form
+        $.ajax({
+            type        : 'POST',
+            url         : $(this).attr("action"), // the url where we want to POST
+            contentType: false,
+            processData: false,
+            data        : formData, // our data object
+            dataType    : 'json' // what type of data do we expect back from the server
+        
+            
+        }).done(function(data) { // using the done promise callback
+
+            // log data to the console so we can see
+            alert("File size: "+data["size"]+' bytes');
+
+            // here we will handle errors and validation messages
+        });
+        
+        // stop the form from submitting the normal way and refreshing the page
+        //event.preventDefault();
+    
+        return false;
+    });
+});
--- server.js
+++ server.js
@@ -8,11 +8,13 @@ var app = express()
 
 app.use(express.static(path.join(__dirname,"public")))
 
-app.post('/upload', upload.single('image-file'), function (req, res, next) {
-    console.log("Firstname: "+ req.body.firstname)
-    res.send('File Size: ' + req.file.size + ' bytes')
-})
+app.get('/jquery/jquery.js', function(req, res) {
+    res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.min.js');
+});
 
+app.post('/upload', upload.single('image-file'), function (req, res, next) {
+    res.send({'size': req.file.size})
+})
 
 app.listen(port, function(){
     console.log("Port listening on: " + port)
